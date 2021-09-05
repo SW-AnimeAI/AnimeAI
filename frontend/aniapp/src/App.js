d2281a4f5a88c21428309e1322c672b0877b63f5
jravel <jac22evar@gmail.com>
2021-09-04 22:07:44 -0500

    componentes

--- frontend/aniapp/src/App.js
+++ frontend/aniapp/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Jacobo from './views/home/home.js'
+import TemporaryDrawer from './views/home/home.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TemporaryDrawer />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <Jacobo/>
+         
           Edit <code>src/App.js</code> ESCUCHO A UNA SEÑORA QUE DICE A.
         </p>
         <a
